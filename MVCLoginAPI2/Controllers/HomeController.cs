2bb35d45ddacacc349de761f6a2d6c376c484e98
Felipe G. Brandão <feeeh@gf2.com.br>
2022-02-25 13:41:58 -0300

    Initial

---
+++ MVCLoginAPI2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLoginAPI2.Models;
+using System.Threading.Tasks;
+
+namespace MVCLoginAPI2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult<Usuario>> Index()
+        {
+            return View();
+        }
+
+        public;
+    }
+}
---
+++ MVCLoginAPI2/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MVCLoginAPI2.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public bool Termos { get; set; }
+    }
+}
